aada01d57fc5a844ea34d47d10ebf50775af8d3a
Matheus Ferraz <99892467+Matheus-FOliveira@users.noreply.github.com>
2025-05-02 01:43:53 -0300

    Work in progress nas controllers

--- src/main/java/matheus/ferraz/srm/controllers/MoedaController.java
+++ src/main/java/matheus/ferraz/srm/controllers/MoedaController.java
@@ -1,4 +1,13 @@
 package matheus.ferraz.srm.controllers;
 
+import matheus.ferraz.srm.services.MoedaService;
+
 public class MoedaController {
+    private final MoedaService moedaService;
+
+    public MoedaController(MoedaService moedaService) {
+        this.moedaService = moedaService;
+    }
+
+
 }
